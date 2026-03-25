64c8e17a0445fbfe26fc9766e959a70d829c39b6
Tejas Saraf <tejas@thekiranacademy.com>
2026-03-25 19:46:34 +0530

    multithreading

---
+++ Multithreading in Java/src/com/basicexample/Task1.java
@@ -0,0 +1,25 @@
+package com.basicexample;
+
+public class Task1 extends Thread{
+
+	void task1() {
+		for (int i = 1; i < 10; i++) {
+
+			System.out.println("task1 --> " + i);
+		}
+	}
+	@Override
+	public void run() {
+
+		for (int i = 1; i < 10; i++) {
+
+			System.out.println("task1 --> " + i);
+		}
+	}
+}
+
+// extends Thread class 
+
+// write the code in overriden run method
+
+// invoke the run method by calling start method
